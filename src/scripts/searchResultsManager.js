764923ad239a52f7f532fbda934987186f9f9220
Trey Suiter <trey.suiter@gmail.com>
2019-10-18 14:48:14 -0500

    completed comments(maybe)

--- src/scripts/searchResultsManager.js
+++ src/scripts/searchResultsManager.js
@@ -1,3 +1,6 @@
+
+//creates function that builds and returns html for recipe search
+
 const buildRecipeHtml = recipe => `
 <article>
   <h4>${recipe.title}</h4>
@@ -8,6 +11,7 @@ const buildRecipeHtml = recipe => `
 </article>
 `
 
+//creates function that takes allRecipes as parameter, set an empty string, applies forEach method to allRecipes, recipe is assigned the value of each iteration of allRecipes, assigns recipeHtml the value of buildRecipeHtml with the recipe parameter, and then accumulates the value of recipeHTml to recipeResultsHTml. It then creates a reference to .search-results and uses the .innterHTML method to assign it to that reference. 
 
 const displayRecipeHtml = allRecipes => {
   let recipeResultsHtml = ""
