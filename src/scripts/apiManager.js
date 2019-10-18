86ec268ce6d6ccd406ba3cf65ed9aaae8358702f
Trey Suiter <trey.suiter@gmail.com>
2019-10-18 14:08:30 -0500

    added comments to event and api manager

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,4 +1,9 @@
+
+// sets variable to base url of api
+
 const apiBaseUrl = "https://www.food2fork.com/api/"
+
+//creates a function that takes the searchInput parameter, api url, and api key and returns the value of the fetch and then applies the json method to it
 
 const searchRecipes = searchInput => fetch(`${apiBaseUrl}search?key=${food2ForkKey}&q=${searchInput}`)
 .then(response => response.json())
